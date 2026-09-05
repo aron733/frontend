926f5e6598cfcec7550074651de73ea3c3665961
aron733 <aronvokouma01@gmail.com>
2026-09-05 22:40:30 +0000

    Remove setPaysAuto

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -19,7 +19,6 @@ function Landing({ onLogin }: { onLogin: (data: any) => void }) {
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [indicatif, setIndicatif] = useState('+226');
-  const [, setPaysAuto] = useState('Burkina Faso');
   const [cookiesAcceptes, setCookiesAcceptes] = useState(localStorage.getItem('cookies_acceptes') === 'true');
   const [showPolitique, setShowPolitique] = useState(false);
 
