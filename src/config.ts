d9b0d710b182c016650325f1194947f4e15b16bf
aron733 <aronvokouma01@gmail.com>
2026-09-06 18:18:53 +0000

    Use production API URL for APK

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
-export const MEDIA_URL = import.meta.env.VITE_API_URL?.replace('/api', '') || 'http://127.0.0.1:8000';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://django-43v1.onrender.com/api';
+export const MEDIA_URL = import.meta.env.VITE_API_URL?.replace('/api', '') || 'https://django-43v1.onrender.com';
