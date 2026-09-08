746eac4a39ff01fabc4b9d8913f7efd1e46d69fd
aron733 <aronvokouma01@gmail.com>
2026-09-08 02:17:53 +0000

    Remove duplicate imports and functions

--- src/pages/Visio.tsx
+++ src/pages/Visio.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useTracks } from '@livekit/components-react';
-import { useTracks } from '@livekit/components-react';
 import axios from 'axios';
 import {
   LiveKitRoom,
@@ -11,19 +10,6 @@ import {
 import '@livekit/components-styles';
 
 import { API_URL } from '../config';
-
-function GrilleParticipants() {
-  const tracks = useTracks([
-    { source: 'camera' as any, withPlaceholder: true },
-    { source: 'screen_share' as any, withPlaceholder: false },
-  ]);
-
-  return (
-    <GridLayout tracks={tracks as any}>
-      <ParticipantTile />
-    </GridLayout>
-  );
-}
 
 function GrilleParticipants() {
   const tracks = useTracks([
